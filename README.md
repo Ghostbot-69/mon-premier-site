86facc831ae8ed7fabaab04aad9ed8a990b85cb0
Ghostbot-69 <ghostdb162@gmail.com>
2025-01-24 18:36:08 +0100

    Mon premier site web

---
+++ README.md
@@ -0,0 +1,11 @@
+# Mon Premier Site Web 🌟  
+Bienvenue sur le site de XxGhostxX !  
+
+### 🛠️ Technologies utilisées :  
+- React.js  
+- HTML / CSS / JavaScript  
+
+### 📬 Mes contacts :  
+- Email : [kami.jrka@gmail.com](mailto:kami.jrka@gmail.com)  
+- Facebook : [XxGhostxX](https://www.facebook.com/8Xx.GHOST.xX8)  
+- GitHub : [Mon GitHub](https://github.com/Ghostbot69)
---
+++ src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>Bienvenue sur le site de Junior (XxGhostxX)</h1>
+        <p>Un site web créé avec passion !</p>
+      </header>
+      <main>
+        <section className="hero">
+          <h2>Contactez-moi</h2>
+          <ul>
+            <li>Email : <a href="mailto:kami.jrka@gmail.com">kami.jrka@gmail.com</a></li>
+            <li>Facebook : <a href="https://www.facebook.com/8Xx.GHOST.xX8" target="_blank">XxGhostxX</a></li>
+            <li>GitHub : <a href="https://github.com/Ghostbot-69" target="_blank">Mon GitHub</a></li>
+          </ul>
+        </section>
+      </main>
+      <footer>
+        <p>© 2025 XxGhostxX. Tous droits réservés.</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
